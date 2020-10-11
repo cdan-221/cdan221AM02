df2883bc22528106c3c838eaf2b30a551ac9ae74
mushroom978 <70561972+mushroom978@users.noreply.github.com>
2020-10-11 16:38:17 -0400

    Scene 2b dark mage

---
+++ Branching Narrative/Assets/Media/Scene 2/script/DialogueScene2b.cs
@@ -0,0 +1,201 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class DialogueScene2b : MonoBehaviour
+{
+    public int primeInt = 1; // This integer drives game progress!
+    public Text Char1name;
+    public Text Char1speech;
+    public Text Char2name;
+    public Text Char2speech;
+    public Text Char3name;
+    public Text Char3speech;
+    public GameObject dialogue;
+    public GameObject ArtChar1;
+    public GameObject ArtChar2;
+    public GameObject ArtBG1;
+    public GameObject Choice1a;
+    public GameObject Choice1b;
+    public GameObject NextScene1Button;
+    public GameObject NextScene2Button;
+    public GameObject nextButton;
+    //public GameObject gameHandler;
+    //public AudioSource audioSource;
+    private bool allowSpace = true;
+
+    void Start()
+    {         // initial visibility settings
+        dialogue.SetActive(false);
+        ArtChar1.SetActive(false);
+        ArtChar2.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    void Update()
+    {         // use spacebar as Next button
+        if (allowSpace == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                talking();
+            }
+        }
+    }
+
+    public void talking()
+    {         // main story function. Players hit next to progress to next int
+        primeInt = primeInt + 1;
+        if (primeInt == 1)
+        {
+            // AudioSource.Play();
+        }
+        else if (primeInt == 2)
+        {
+            ArtChar1.SetActive(true);
+            dialogue.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "???";
+            Char2speech.text = "Amazing! What nice magic essence you have.";
+        }
+        else if (primeInt == 3)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "Huh?! Who are you?!";
+            Char2name.text = "";
+            Char2speech.text = "";
+            //gameHandler.AddPlayerStat(1);
+        }
+        else if (primeInt == 4)
+        {
+            Char1name.text = "";
+            Char1speech.text = ".";
+            Char2name.text = "Greg";
+            Char2speech.text = "The name's Greg, I'm just another Dark Mage taking a stroll.";
+        }
+        else if (primeInt == 5)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "Really? A Stroll? Here?";
+            Char2name.text = "";
+            Char2speech.text = "?";
+            //gameHandler.AddPlayerStat(1);
+        }
+        else if (primeInt == 6)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Greg";
+            Char2speech.text = "Everyone has their own preference you know.";
+        }
+        else if (primeInt == 7)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Greg";
+            Char2speech.text = "Besides that I see a magic essence inside you great in power! But it seems lost, you haven't chosen a path yet have you?";
+        }
+        else if (primeInt == 8)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "...no, not yet.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 9)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Greg";
+            Char2speech.text = "Well let me help you decide. Right here use this spell to ressurect this skeleton.";
+            // Turn off "Next" button, turn on "Choice" buttons
+            nextButton.SetActive(false);
+            allowSpace = false;
+            Choice1a.SetActive(true); // function Choice1aFunct()
+            Choice1b.SetActive(true); // function Choice1bFunct()
+        }
+        // ENCOUNTER AFTER CHOICE #1
+        else if (primeInt == 100)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Greg";
+            Char2speech.text = "Cool right?!";
+        }
+        else if (primeInt == 101)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Greg";
+            Char2speech.text = "If you follow the Dark mages path you can do all sorts of “Fun”. Well I must go now. Follow the path on the right if you're interested in being a one of the cool guys. Take care of Smooth Criminal over there will ya.";
+            nextButton.SetActive(false);
+            allowSpace = false;
+            NextScene1Button.SetActive(true);
+            NextScene2Button.SetActive(true);
+        }
+
+        else if (primeInt == 200)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Greg";
+            Char2speech.text = "You're a Square, no squares allowed in the Dark Mage's club.";
+        }
+        else if (primeInt == 201)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Greg";
+            Char2speech.text = "Out of my sight, walk the left path. I don't wanna see a boring person like you anywhere around me!";
+            nextButton.SetActive(false);
+            allowSpace = false;
+            NextScene1Button.SetActive(true);
+            NextScene2Button.SetActive(true);
+        }
+    }
+
+    // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
+    public void Choice1aFunct()
+    {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "Skeleton";
+        Char2speech.text = "Hee Hee!";
+        primeInt = 99;
+        ArtChar2.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+    public void Choice1bFunct()
+    {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "Greg";
+        Char2speech.text = "BORING!";
+        primeInt = 199;
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+
+    public void SceneChange2a()
+    {
+        SceneManager.LoadScene("Scene2a");
+    }
+    public void SceneChange2b()
+    {
+        SceneManager.LoadScene("Scene2b");
+    }
+}
